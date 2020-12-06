40f7125e4384784fbe212e21c5322e4d6f3802a7
wanderxuhq <wanderxuhq@gmail.com>
2020-12-06 20:20:36 +0800

    调整代码结构

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    let input = String::from("啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
-    //let input = String::from("Hello World");
+    let input = String::from(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=",
+    );
     println!("{}", input);
-    let mut bytes = Vec::from(input.as_bytes());
+    let bytes = Vec::from(input.as_bytes());
+    println!("{}", md5(bytes));
+}
+
+fn md5(mut bytes: Vec<u8>) -> String {
     let len = bytes.len();
     if len % 64 != 56 {
         bytes.push(1 << 7);
@@ -11,134 +16,117 @@ fn main() {
         }
     }
     let size = len * 8;
-    println!("{:?}", len);
-    println!("{:?}", size.to_le_bytes());
     bytes.extend_from_slice(&size.to_le_bytes());
     bytes.push(0);
     bytes.push(0);
     bytes.push(0);
     bytes.push(0);
-    let mut a = 0x67452301 as u32;
-    let mut b = 0xEFCDAB89 as u32;
-    let mut c = 0x98BADCFE as u32;
-    let mut d = 0x10325476 as u32;
-    let mut A = 0x67452301 as u32;
-    let mut B = 0xEFCDAB89 as u32;
-    let mut C = 0x98BADCFE as u32;
-    let mut D = 0x10325476 as u32;
+
+    let (mut a, mut b, mut c, mut d) = (
+        0x67452301 as u32,
+        0xEFCDAB89 as u32,
+        0x98BADCFE as u32,
+        0x10325476 as u32,
+    );
     for i in 0..bytes.len() / 64 {
-        a = A;
-        b = B;
-        c = C;
-        d = D;
-        println!("{}", i);
-        let bslice = &bytes[i * 64..i * 64 + 64];
-        let bs = [
-            tou32(&bslice[0..4]),
-            tou32(&bslice[4..8]),
-            tou32(&bslice[8..12]),
-            tou32(&bslice[12..16]),
-            tou32(&bslice[16..20]),
-            tou32(&bslice[20..24]),
-            tou32(&bslice[24..28]),
-            tou32(&bslice[28..32]),
-            tou32(&bslice[32..36]),
-            tou32(&bslice[36..40]),
-            tou32(&bslice[40..44]),
-            tou32(&bslice[44..48]),
-            tou32(&bslice[48..52]),
-            tou32(&bslice[52..56]),
-            tou32(&bslice[56..60]),
-            tou32(&bslice[60..64]),
-        ];
-        a = ff(a, b, c, d, bs[0], 7, 0xd76aa478);
-        d = ff(d, a, b, c, bs[1], 12, 0xe8c7b756);
-        c = ff(c, d, a, b, bs[2], 17, 0x242070db);
-        b = ff(b, c, d, a, bs[3], 22, 0xc1bdceee);
-        a = ff(a, b, c, d, bs[4], 7, 0xf57c0faf);
-        d = ff(d, a, b, c, bs[5], 12, 0x4787c62a);
-        c = ff(c, d, a, b, bs[6], 17, 0xa8304613);
-        b = ff(b, c, d, a, bs[7], 22, 0xfd469501);
-        a = ff(a, b, c, d, bs[8], 7, 0x698098d8);
-        d = ff(d, a, b, c, bs[9], 12, 0x8b44f7af);
-        c = ff(c, d, a, b, bs[10], 17, 0xffff5bb1);
-        b = ff(b, c, d, a, bs[11], 22, 0x895cd7be);
-        a = ff(a, b, c, d, bs[12], 7, 0x6b901122);
-        d = ff(d, a, b, c, bs[13], 12, 0xfd987193);
-        c = ff(c, d, a, b, bs[14], 17, 0xa679438e);
-        b = ff(b, c, d, a, bs[15], 22, 0x49b40821);
+        let (mut _a, mut _b, mut _c, mut _d) = (a, b, c, d);
+        let bs = |bslice: &[u8]| -> Vec<u32> {
+            let mut r = Vec::new();
+            for i in 0..64 / 4 {
+                r.push(
+                    ((bslice[i * 4 + 3] as u32) << 24)
+                        + ((bslice[i * 4 + 2] as u32) << 16)
+                        + ((bslice[i * 4 + 1] as u32) << 8)
+                        + (bslice[i * 4] as u32),
+                );
+            }
+            r
+        }(&bytes[i * 64..i * 64 + 64]);
 
-        a = gg(a, b, c, d, bs[1], 5, 0xf61e2562);
-        d = gg(d, a, b, c, bs[6], 9, 0xc040b340);
-        c = gg(c, d, a, b, bs[11], 14, 0x265e5a51);
-        b = gg(b, c, d, a, bs[0], 20, 0xe9b6c7aa);
-        a = gg(a, b, c, d, bs[5], 5, 0xd62f105d);
-        d = gg(d, a, b, c, bs[10], 9, 0x02441453);
-        c = gg(c, d, a, b, bs[15], 14, 0xd8a1e681);
-        b = gg(b, c, d, a, bs[4], 20, 0xe7d3fbc8);
-        a = gg(a, b, c, d, bs[9], 5, 0x21e1cde6);
-        d = gg(d, a, b, c, bs[14], 9, 0xc33707d6);
-        c = gg(c, d, a, b, bs[3], 14, 0xf4d50d87);
-        b = gg(b, c, d, a, bs[8], 20, 0x455a14ed);
-        a = gg(a, b, c, d, bs[13], 5, 0xa9e3e905);
-        d = gg(d, a, b, c, bs[2], 9, 0xfcefa3f8);
-        c = gg(c, d, a, b, bs[7], 14, 0x676f02d9);
-        b = gg(b, c, d, a, bs[12], 20, 0x8d2a4c8a);
+        _a = ff(_a, _b, _c, _d, bs[0], 7, 0xd76aa478);
+        _d = ff(_d, _a, _b, _c, bs[1], 12, 0xe8c7b756);
+        _c = ff(_c, _d, _a, _b, bs[2], 17, 0x242070db);
+        _b = ff(_b, _c, _d, _a, bs[3], 22, 0xc1bdceee);
+        _a = ff(_a, _b, _c, _d, bs[4], 7, 0xf57c0faf);
+        _d = ff(_d, _a, _b, _c, bs[5], 12, 0x4787c62a);
+        _c = ff(_c, _d, _a, _b, bs[6], 17, 0xa8304613);
+        _b = ff(_b, _c, _d, _a, bs[7], 22, 0xfd469501);
+        _a = ff(_a, _b, _c, _d, bs[8], 7, 0x698098d8);
+        _d = ff(_d, _a, _b, _c, bs[9], 12, 0x8b44f7af);
+        _c = ff(_c, _d, _a, _b, bs[10], 17, 0xffff5bb1);
+        _b = ff(_b, _c, _d, _a, bs[11], 22, 0x895cd7be);
+        _a = ff(_a, _b, _c, _d, bs[12], 7, 0x6b901122);
+        _d = ff(_d, _a, _b, _c, bs[13], 12, 0xfd987193);
+        _c = ff(_c, _d, _a, _b, bs[14], 17, 0xa679438e);
+        _b = ff(_b, _c, _d, _a, bs[15], 22, 0x49b40821);
 
-        a = hh(a, b, c, d, bs[5], 4, 0xfffa3942);
-        d = hh(d, a, b, c, bs[8], 11, 0x8771f681);
-        c = hh(c, d, a, b, bs[11], 16, 0x6d9d6122);
-        b = hh(b, c, d, a, bs[14], 23, 0xfde5380c);
-        a = hh(a, b, c, d, bs[1], 4, 0xa4beea44);
-        d = hh(d, a, b, c, bs[4], 11, 0x4bdecfa9);
-        c = hh(c, d, a, b, bs[7], 16, 0xf6bb4b60);
-        b = hh(b, c, d, a, bs[10], 23, 0xbebfbc70);
-        a = hh(a, b, c, d, bs[13], 4, 0x289b7ec6);
-        d = hh(d, a, b, c, bs[0], 11, 0xeaa127fa);
-        c = hh(c, d, a, b, bs[3], 16, 0xd4ef3085);
-        b = hh(b, c, d, a, bs[6], 23, 0x04881d05);
-        a = hh(a, b, c, d, bs[9], 4, 0xd9d4d039);
-        d = hh(d, a, b, c, bs[12], 11, 0xe6db99e5);
-        c = hh(c, d, a, b, bs[15], 16, 0x1fa27cf8);
-        b = hh(b, c, d, a, bs[2], 23, 0xc4ac5665);
+        _a = gg(_a, _b, _c, _d, bs[1], 5, 0xf61e2562);
+        _d = gg(_d, _a, _b, _c, bs[6], 9, 0xc040b340);
+        _c = gg(_c, _d, _a, _b, bs[11], 14, 0x265e5a51);
+        _b = gg(_b, _c, _d, _a, bs[0], 20, 0xe9b6c7aa);
+        _a = gg(_a, _b, _c, _d, bs[5], 5, 0xd62f105d);
+        _d = gg(_d, _a, _b, _c, bs[10], 9, 0x02441453);
+        _c = gg(_c, _d, _a, _b, bs[15], 14, 0xd8a1e681);
+        _b = gg(_b, _c, _d, _a, bs[4], 20, 0xe7d3fbc8);
+        _a = gg(_a, _b, _c, _d, bs[9], 5, 0x21e1cde6);
+        _d = gg(_d, _a, _b, _c, bs[14], 9, 0xc33707d6);
+        _c = gg(_c, _d, _a, _b, bs[3], 14, 0xf4d50d87);
+        _b = gg(_b, _c, _d, _a, bs[8], 20, 0x455a14ed);
+        _a = gg(_a, _b, _c, _d, bs[13], 5, 0xa9e3e905);
+        _d = gg(_d, _a, _b, _c, bs[2], 9, 0xfcefa3f8);
+        _c = gg(_c, _d, _a, _b, bs[7], 14, 0x676f02d9);
+        _b = gg(_b, _c, _d, _a, bs[12], 20, 0x8d2a4c8a);
 
-        a = ii(a, b, c, d, bs[0], 6, 0xf4292244);
-        d = ii(d, a, b, c, bs[7], 10, 0x432aff97);
-        c = ii(c, d, a, b, bs[14], 15, 0xab9423a7);
-        b = ii(b, c, d, a, bs[5], 21, 0xfc93a039);
-        a = ii(a, b, c, d, bs[12], 6, 0x655b59c3);
-        d = ii(d, a, b, c, bs[3], 10, 0x8f0ccc92);
-        c = ii(c, d, a, b, bs[10], 15, 0xffeff47d);
-        b = ii(b, c, d, a, bs[1], 21, 0x85845dd1);
-        a = ii(a, b, c, d, bs[8], 6, 0x6fa87e4f);
-        d = ii(d, a, b, c, bs[15], 10, 0xfe2ce6e0);
-        c = ii(c, d, a, b, bs[6], 15, 0xa3014314);
-        b = ii(b, c, d, a, bs[13], 21, 0x4e0811a1);
-        a = ii(a, b, c, d, bs[4], 6, 0xf7537e82);
-        d = ii(d, a, b, c, bs[11], 10, 0xbd3af235);
-        c = ii(c, d, a, b, bs[2], 15, 0x2ad7d2bb);
-        b = ii(b, c, d, a, bs[9], 21, 0xeb86d391);
-        A = A.wrapping_add(a);
-        B = B.wrapping_add(b);
-        C = C.wrapping_add(c);
-        D = D.wrapping_add(d);
+        _a = hh(_a, _b, _c, _d, bs[5], 4, 0xfffa3942);
+        _d = hh(_d, _a, _b, _c, bs[8], 11, 0x8771f681);
+        _c = hh(_c, _d, _a, _b, bs[11], 16, 0x6d9d6122);
+        _b = hh(_b, _c, _d, _a, bs[14], 23, 0xfde5380c);
+        _a = hh(_a, _b, _c, _d, bs[1], 4, 0xa4beea44);
+        _d = hh(_d, _a, _b, _c, bs[4], 11, 0x4bdecfa9);
+        _c = hh(_c, _d, _a, _b, bs[7], 16, 0xf6bb4b60);
+        _b = hh(_b, _c, _d, _a, bs[10], 23, 0xbebfbc70);
+        _a = hh(_a, _b, _c, _d, bs[13], 4, 0x289b7ec6);
+        _d = hh(_d, _a, _b, _c, bs[0], 11, 0xeaa127fa);
+        _c = hh(_c, _d, _a, _b, bs[3], 16, 0xd4ef3085);
+        _b = hh(_b, _c, _d, _a, bs[6], 23, 0x04881d05);
+        _a = hh(_a, _b, _c, _d, bs[9], 4, 0xd9d4d039);
+        _d = hh(_d, _a, _b, _c, bs[12], 11, 0xe6db99e5);
+        _c = hh(_c, _d, _a, _b, bs[15], 16, 0x1fa27cf8);
+        _b = hh(_b, _c, _d, _a, bs[2], 23, 0xc4ac5665);
 
+        _a = ii(_a, _b, _c, _d, bs[0], 6, 0xf4292244);
+        _d = ii(_d, _a, _b, _c, bs[7], 10, 0x432aff97);
+        _c = ii(_c, _d, _a, _b, bs[14], 15, 0xab9423a7);
+        _b = ii(_b, _c, _d, _a, bs[5], 21, 0xfc93a039);
+        _a = ii(_a, _b, _c, _d, bs[12], 6, 0x655b59c3);
+        _d = ii(_d, _a, _b, _c, bs[3], 10, 0x8f0ccc92);
+        _c = ii(_c, _d, _a, _b, bs[10], 15, 0xffeff47d);
+        _b = ii(_b, _c, _d, _a, bs[1], 21, 0x85845dd1);
+        _a = ii(_a, _b, _c, _d, bs[8], 6, 0x6fa87e4f);
+        _d = ii(_d, _a, _b, _c, bs[15], 10, 0xfe2ce6e0);
+        _c = ii(_c, _d, _a, _b, bs[6], 15, 0xa3014314);
+        _b = ii(_b, _c, _d, _a, bs[13], 21, 0x4e0811a1);
+        _a = ii(_a, _b, _c, _d, bs[4], 6, 0xf7537e82);
+        _d = ii(_d, _a, _b, _c, bs[11], 10, 0xbd3af235);
+        _c = ii(_c, _d, _a, _b, bs[2], 15, 0x2ad7d2bb);
+        _b = ii(_b, _c, _d, _a, bs[9], 21, 0xeb86d391);
+
+        a = a.wrapping_add(_a);
+        b = b.wrapping_add(_b);
+        c = c.wrapping_add(_c);
+        d = d.wrapping_add(_d);
     }
 
-    println!(
+    format!(
         "{:X} {:X} {:X} {:X}",
-        reverse_byte(A),
-        reverse_byte(B),
-        reverse_byte(C),
-        reverse_byte(D)
-    );
+        reverse_bytes(a),
+        reverse_bytes(b),
+        reverse_bytes(c),
+        reverse_bytes(d)
+    )
 }
 
-fn tou32(b: &[u8]) -> u32 {
-    ((b[3] as u32) << 24) + ((b[2] as u32) << 16) + ((b[1] as u32) << 8) + (b[0] as u32)
-}
-
-fn reverse_byte(b: u32) -> u32 {
+fn reverse_bytes(b: u32) -> u32 {
     ((b & 0x000000FF) << 24)
         | ((b & 0x0000FF00) << 8)
         | ((b & 0x00FF0000) >> 8)
